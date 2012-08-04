fbfb79ada5edc32583faea86cf04f32bec9bfe29
Robert Blake <rob.c.blake.3@gmail.com>
2012-08-04 19:53:46 -0400

    Removing the non-printing characters from speech.

--- xchat-speak.py
+++ xchat-speak.py
@@ -168,6 +168,11 @@ class xchat_speak:
         return os.path.join(xchat.get_info("xchatdir"),"pronounciation_database.pickle")
 
     def clean(self,message):
+        bad_chars = ( "\x00\x01\x02\x03\x04\x05\x06\x07\x08\x09\x0A\x0B\x0C\x0D\x0E\x0F"
+                     +"\x10\x11\x12\x13\x14\x15\x16\x17\x18\x19\x1A\x1B\x1C\x1D\x1E\x1F"
+                     )
+        message = message.translate(None, bad_chars)
+
         words = message.split()
         cleaned = []
         for word in words:
